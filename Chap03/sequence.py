284adeed7e2a297870e0d2a8e65fa00b6ec1d1bb
Sanjay <yella016@github.umn.edu>
2019-07-09 01:40:35 -0700

    Chapter 03 code

---
+++ Chap03/sequence.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+__author__ = "Sanjay Yellambalase Ravikumar"
+
+x = [ 1, 2, 3, 4, 5 ]
+for i in x:
+    print('i is {}'.format(i))
---
+++ Chap03/types.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+__author__ = "Sanjay Yellambalase Ravikumar"
+
+x = 7
+print('x is {}'.format(x))
+print(type(x))
