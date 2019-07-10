d0222cef280d11084934e84e3d7f1623cedcbf77
Sanjay <yella016@github.umn.edu>
2019-07-10 00:16:18 -0700

    Chaper 06 code

---
+++ Chap06/for.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+__author__ = "Sanjay Yellambalase Ravikumar"
+
+animals = ( 'bear', 'bunny', 'dog', 'cat', 'velociraptor' )
+
+for pet in animals:
+    print(pet)
+
+"""
+
+break
+continue
+else
+
+"""
---
+++ Chap06/while.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+__author__ = "Sanjay Yellambalase Ravikumar"
+
+secret = 'swordfish'
+pw = ''
+
+while pw != secret:
+    pw = input("What's the secret word? ")
+
+
+
